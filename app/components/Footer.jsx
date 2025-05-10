1ed24d1dc44f23922a6253f03162f26de45ae690
Lester del Rosario <79028758+Lester1105@users.noreply.github.com>
2025-05-10 22:50:56 +0800

    commit changes

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = ({isDarkMode}) => {
 
             <div className='text-center sm:flex items-center justify-between border-t
             border-gray-400 mx-[10%] mt-12 py-6'>
-                <p>© Lester Del Rosario. All rights reserved.</p>
+                <p>© 2025 Lester Del Rosario. All rights reserved.</p>
                 <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
                     <li><a target='_blank' href="https://github.com/Lester1105">Github</a></li>
                     <li><a target='_blank' href="https://www.linkedin.com/in/lester-del-rosario-a83846a5/">LinkedIn</a></li>
